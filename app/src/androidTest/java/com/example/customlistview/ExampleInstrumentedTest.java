41689458299cab9c97cd1b3c0e84db61369b4638
GOKU <gannarala@gmail.com>
2019-10-04 04:59:20 +0200

    CustomListView

---
+++ app/src/androidTest/java/com/example/customlistview/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.example.customlistview;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented list_item, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under list_item.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.customlistview", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/com/example/customlistview/Adapter_list.java
@@ -0,0 +1,46 @@
+package com.example.customlistview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Adapter_list extends ArrayAdapter
+{
+    private int[] imageList;
+    private String[] nameList;
+    private String[] descriptionList;
+
+    public Adapter_list(Context context, int[] imageList, String[] nameList, String[] descriptionList)
+    {
+        super(context, 0, nameList);
+        this.imageList = imageList;
+        this.nameList = nameList;
+        this.descriptionList = descriptionList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        if (convertView == null)
+        {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.list_item, parent, false);
+        }
+
+        ImageView imageView = convertView.findViewById(R.id.image_view);
+        TextView textView1 = convertView.findViewById(R.id.text_view1);
+        TextView textView2 = convertView.findViewById(R.id.textView2);
+
+        imageView.setImageResource(imageList[position]);
+        textView1.setText(nameList[position]);
+        textView2.setText(descriptionList[position]);
+
+        return convertView;
+    }
+}
